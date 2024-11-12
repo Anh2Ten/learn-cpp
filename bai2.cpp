2744e28740b3ecaa1b8201ad3784e156eea70c2c
Lê Đức Anh <83670932+Anh2Ten@users.noreply.github.com>
2024-11-12 18:51:38 +0700

    Create bai2.cpp

---
+++ bai2.cpp
@@ -0,0 +1,15 @@
+//Bài 2: Tính S(n) = 1^2 + 2^2 + … + n^2
+ 
+include <iostream>
+using namespace std;
+
+int main() {
+  int n;
+  cout << "nhap n";
+  cin >> n;
+  int ketqua = 0;
+  for(int i = 1; i <= n; i++) {
+    ketqua += i * i;
+  }
+  cout << ketqua;
+}
